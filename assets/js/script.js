3d584a50b54de9e42d421263adfee44731583ef2
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-10-12 00:35:50 +0100

    jumbotron background color

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,28 @@
 const dateContainer = $("#currentDay");
 
+// render date and time display
 const onReady = function () {
   const timerTick = function () {
     const dateTime = moment();
-    const dateTimeFormatted = dateTime.format("MMM Do YYYY, h:mm a");
+    const dateTimeFormatted = dateTime.format("dddd, MMM Do YYYY, h:mm a");
 
     dateContainer.text(dateTimeFormatted);
   };
   const timer = setInterval(timerTick, 1000);
 };
 
-$(document).ready(onReady);
+const renderTimeBlocks = function () {
+  console.log("time blocks rendered");
+};
 
-// $(document).load();
+// on load:
+//  get from local storage
+// render timeblocks and text content
+
+// LS
+// keys by hour
+
+// click event on save button (use target and event capturing)
+
+$(document).ready(onReady);
+$(document).ready(renderTimeBlocks);
